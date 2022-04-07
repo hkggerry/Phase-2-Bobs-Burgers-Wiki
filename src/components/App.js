d2960066eb18813ed2af6f29bd9441294ab559bc
gerry <hkggerry@gmail.com>
2022-04-07 11:13:41 -0700

    Combined all Seasons component in one

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,6 @@ import NavBar from "./NavBar"
 import Home from "./HomePage";
 import Characters from "./Characters";
 import SuggestionBox from "./SuggestionBox";
-// import Season1 from "./Seasons/Season1"
-// import Season2 from "./Seasons/Season2"
-// import Season3 from "./Seasons/Season3"
-// import Season4 from "./Seasons/Season4"
-// import Season5 from "./Seasons/Season5"
-// import Season6 from "./Seasons/Season6"
-// import Season7 from "./Seasons/Season7"
-// import Season8 from "./Seasons/Season8"
-// import Season9 from "./Seasons/Season9"
-// import Season10 from "./Seasons/Season10"
-// import Season11 from "./Seasons/Season11"
-// import Season12 from "./Seasons/Season12"
 import Season from "./Season";
 
 function App() {
@@ -68,43 +56,6 @@ function App() {
           <Route exact path="/season/:id" >
             <Season episodes={episodes}/>
           </Route>
-
-          {/* <Route exact path="/season1" >
-            <Season1 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season2" >
-            <Season2 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season3" >
-            <Season3 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season4" >
-            <Season4 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season5" >
-            <Season5 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season6" >
-            <Season6 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season7" >
-            <Season7 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season8" >
-            <Season8 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season9" >
-            <Season9 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season10" >
-            <Season10 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season11" >
-            <Season11 episodes={episodes}/>
-          </Route>
-          <Route exact path="/season12" >
-            <Season12 episodes={episodes}/>
-          </Route> */}
         </Switch>
     </div>
   );
--- src/components/Season.js
+++ src/components/Season.js
@@ -3,26 +3,35 @@ import { useParams } from "react-router-dom";
 
 function Season({episodes}){
 
-    const { id } = useParams();
+    const { id } = useParams("");
     const seasonList = episodes.map(episode => <li>{episode.name}</li>)
 
-    // const print = () =>{
-    //     if (id = 1){
-    //         return seasonList.slice(0,13)
-    //     }else if (id=2){
-    //         return seasonList.slice(13,22)
-    //     }
-    // }
-    // const seasonEp = seasonList.slice(0,13)
-
-    // const seasonList = episodes.map(episode => {
-    //     if (episode.season = 1) {
-    //         <li>{episode.name.slice(0,13)}</li>
-    //     }else if (episode.season = 2) {
-    //         <li>{episode.name.slice(13,22)}</li>
-    //     }
-    // })
-
+        if ( id == 1){
+            return seasonList.slice(0,13)
+        }else if (id == 2){
+            return seasonList.slice(13,22)
+        }else if (id == 3){
+            return seasonList.slice(22,45)
+        }else if (id == 4){
+            return seasonList.slice(45,67)
+        }else if (id == 5){
+            return seasonList.slice(67,88)
+        }else if (id == 6){
+            return seasonList.slice(88,107)
+        }else if (id == 7){
+            return seasonList.slice(107,128)
+        }else if (id == 8){
+            return seasonList.slice(128,150)
+        }else if (id == 9){
+            return seasonList.slice(150,172)
+        }else if (id == 10){
+            return seasonList.slice(172,194)
+        }else if (id == 11){
+            return seasonList.slice(194,216)
+        }else if (id == 12){
+            return seasonList.slice(216,228)
+        }
+        
     return(
         <div>
             <h2>Season: {id}</h2>
